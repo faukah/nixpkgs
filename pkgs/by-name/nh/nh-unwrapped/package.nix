f927ed96d1a3d1ea07a4df8bd1e10146e9f7d40c
faukah <fau@faukah.com>
2026-02-25 10:22:16 +0100

    nh-unwrapped: 4.2 -> 4.3

--- pkgs/by-name/nh/nh-unwrapped/package.nix
+++ pkgs/by-name/nh/nh-unwrapped/package.nix
@@ -4,13 +4,12 @@
   rustPlatform,
   installShellFiles,
   fetchFromGitHub,
-  fetchpatch,
   nix-update-script,
   buildPackages,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nh-unwrapped";
-  version = "4.2.0"; # Did you remove the patch below (and this comment)?
+  version = "4.3.0";
 
   src = fetchFromGitHub {
     owner = "nix-community";
@@ -18,13 +17,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-6n5SVO8zsdVTD691lri7ZcO4zpqYFU8GIvjI6dbxkA8=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/nix-community/nh/commit/8bf323483166797a204579a43ed8810113eb128c.patch";
-      hash = "sha256-hg0LgDPjiPWR+1DRzqORv6QPlrds7ys4PTDXFw6PUoI=";
-    })
-  ];
 
   strictDeps = true;
 
